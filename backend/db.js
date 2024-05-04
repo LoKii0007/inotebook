e2396510afadce15d142dd3319c78633480eea43
Lokii0007 <lokeshyadav1098@gmail.com>
2024-05-04 19:13:05 +0530

    akjns

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 // const mongoURI='mongodb://127.0.0.1:27017/inotebook'
-const mongoURI='mongodb+srv://lokeshyadav1098:ytyS3En2kPSCich1@cluster0.z6dp4wl.mongodb.net/imagnify?retryWrites=true&w=majority'
 
 const connectToMongo = async () => {
     try {
